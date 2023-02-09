db27cdd42c6583c94b95bc8ee98a84a1d2e59d3b
germanrogu <german.rodriguezgu@gmail.com>
2023-02-09 11:41:38 -0500

    Delete css classes

--- src/components/atoms/FieldBox.tsx
+++ src/components/atoms/FieldBox.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const FieldBox = ({ value, status }: Props) => {
   const classes = classNames(
-    "w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white",
+    "text-black w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white",
     {
       "bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600":
         !status,
