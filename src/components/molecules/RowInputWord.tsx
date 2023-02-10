727645bccc7050c3d8e0797692a8d23e68b9778d
germanrogu <german.rodriguezgu@gmail.com>
2023-02-10 11:22:00 -0500

    Handel on enter event

--- src/components/molecules/RowInputWord.tsx
+++ src/components/molecules/RowInputWord.tsx
@@ -4,13 +4,23 @@ import { FieldBox } from "../atoms/FieldBox";
 interface Props {
   inputWord: string;
   wordSolution: string;
+  onEnterKey: () => void;
 }
 
-export const RowInputWord = ({ inputWord, wordSolution }: Props) => {
+export const RowInputWord = ({
+  inputWord,
+  wordSolution,
+  onEnterKey,
+}: Props) => {
   const count = wordSolution.length - inputWord.split("").length;
   const fieldsEmpty = Array.from(Array(count)).map((_, i) => (
     <FieldBox key={i} />
   ));
+
+  if (inputWord.length === 5)
+    setTimeout(() => {
+      onEnterKey();
+    }, 400);
 
   return (
     <div className={"flex justify-center mb-1"}>
