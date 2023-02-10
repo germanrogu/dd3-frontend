d9a9be144edb7b741f9ef6d80b5c17cc9622ce15
germanroalgoritmo <german.rodriguez@algoritmo.co>
2023-02-10 06:59:33 -0500

    :recycle: To handle styles of field box

--- src/components/atoms/FieldBox.tsx
+++ src/components/atoms/FieldBox.tsx
@@ -3,30 +3,27 @@ import React from "react";
 
 interface Props {
   value?: string;
-  status?: "absent" | "present" | "correct";
+  status?: "nada" | "presente" | "correcto";
 }
 
 export const FieldBox = ({ value, status }: Props) => {
-  const classes = classNames(
-    "text-black w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white",
-    {
-      "bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600":
-        !status,
-      "border-black dark:border-slate-100": value && !status,
-      "absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700":
-        status === "absent",
-      "correct shadowed bg-green-500 text-white border-green-500":
-        status === "correct",
-      "present shadowed bg-yellow-500 text-white border-yellow-500":
-        status === "present",
-    }
-  );
+  const baseClasses =
+    "text-black w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded";
+  const darkClasses = "dark:text-white dark:bg-zinc-600 dark:border-zinc-600";
+
+  const classes = classNames(baseClasses, darkClasses, {
+    "bg-white border-slate-200": !status,
+    "border-black": value && !status,
+    "bg-green-600 text-white border-green-600 shadowed": status === "correcto",
+    "bg-slate-400 text-white border-slate-400 shadowed": status === "nada",
+    "bg-yellow-400 text-white border-yellow-400 shadowed":
+      status === "presente",
+    "dark:bg-slate-700 dark:border-slate-700": status === "nada",
+  });
 
   return (
-    <div className={classes} style={{ animationDelay: "100ms" }}>
-      <div className="letter-container" style={{ animationDelay: "100ms" }}>
-        {value}
-      </div>
+    <div className={classes}>
+      <div>{value}</div>
     </div>
   );
 };
