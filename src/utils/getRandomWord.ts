5c369d6c4fe692bd4074f1bd89426394d0ce4abc
germanrogu <german.rodriguezgu@gmail.com>
2023-02-10 10:08:20 -0500

    Remove accents f word

--- src/utils/getRandomWord.ts
+++ src/utils/getRandomWord.ts
@@ -1,6 +1,11 @@
 import { DICTIONARY } from "./constants/Dictionary";
+import { GRID_GAMES_VALUE } from "./constants/values";
 
 const getRandomInt = (max: number) => Math.floor(Math.random() * max);
+
+const removeAccents = (str: string) => {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
 
 const getRandomWord = (): Promise<string> => {
   const index = getRandomInt(DICTIONARY.length - 1);
@@ -11,8 +16,8 @@ const getRandomWord = (): Promise<string> => {
   return new Promise<string>((resolve) => {
     return resolve(word);
   }).then((word: string) => {
-    if (word.length === 5) {
-      return word;
+    if (word.length === GRID_GAMES_VALUE) {
+      return removeAccents(word);
     } else {
       return getRandomWord();
     }
